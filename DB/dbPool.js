961a6afc2ad97a17768bd5ed9da857411e4cf00d
Kim Donghyun <usccollege1202@gmail.com>
2022-09-15 20:38:17 +0900

    DB connection pool added (.env will be added later)

---
+++ DB/dbPool.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "123456789",
+  database: "complexdb",
+  connectionLimit: 10
+});
+
+module.exports = pool;
