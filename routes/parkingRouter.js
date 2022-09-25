fdad818b6d3d7f6886736feddbe9da1ebd4a6504
Kim Donghyun <usccollege1202@gmail.com>
2022-09-25 19:32:19 +0900

    입출차 정보 조회 추가

--- routes/parkingRouter.js
+++ routes/parkingRouter.js
@@ -105,4 +105,77 @@ router.get("/getDetailedCarLocationList", async (req, res, next) => {
   }
 });
 
+// 입출차 정보 조회
+router.get("/getParkingIOList", async (req, res, next) => {
+  let {
+    startTime = "",
+    endTime = "",
+    dongCode = "",
+    hoCode = "",
+    sendResult = "",
+    carNo = ""
+  } = req.query;
+  console.log(startTime, endTime, dongCode, hoCode, sendResult, carNo);
+
+  try {
+    let defaultCondition = `LIKE '%'`;
+    let defaultStartDateCondition = "";
+    let defaultEndDateCondition = "";
+    let defaultDongCondition = defaultCondition;
+    let defaultHoCondition = defaultCondition;
+    let defaultSendResultCondition = defaultCondition;
+    let defaultCarNoCondition = defaultCondition;
+
+    let dongCondition = "";
+    let hoCondition = "";
+    let sendResultCondition = "";
+    let carNoCondition = "";
+
+    if (!startTime) {
+      defaultStartDateCondition = "1900-01-01";
+    }
+    if (!endTime) {
+      defaultEndDateCondition = "3000-01-01";
+    }
+    if (!!dongCode) {
+      dongCondition = `= '${dongCode}'`;
+      defaultDongCondition = "";
+    }
+    if (!!hoCode) {
+      hoCondition = `= '${hoCode}'`;
+      defaultHoCondition = "";
+    }
+    if (!!sendResult) {
+      sendResultCondition = `= '${sendResult}'`;
+      defaultSendResultCondition = "";
+    }
+    if(!!carNo){
+      carNoCondition = `= '${carNo}'`;
+      defaultCarNoCondition = "";
+    }
+    const sql = `SELECT ROW_NUMBER() OVER(ORDER BY idx) AS No, inout_dtime AS inoutDtime, 
+                        inout_flag AS inoutFlag, dong_code AS dongCode, ho_code AS hoCode
+                        car_no AS carNo, send_time AS sendTime, send_result AS sendResult
+                 FROM t_parking_io
+                 WHERE (DATE(inout_dtime) >= '${defaultStartDateCondition} ${startTime}' AND DATE(inout_dtime) <= '${defaultEndDateCondition} ${endTime}')
+                       AND (dong_code ${defaultDongCondition} ${dongCondition} AND ho_code ${defaultHoCondition} ${hoCondition})
+                       AND send_result ${defaultSendResultCondition} ${sendResultCondition}
+                       AND car_no ${defaultCarNoCondition} ${carNoCondition}`;
+    const data = await pool.query(sql);
+    console.log("sql: " + sql);
+    let resultList = data[0];
+                   
+    let jsonResult = {
+      resultCode: "00",
+      resultMsg: "NORMAL_SERVICE",
+      data: {
+      items: resultList,
+      },
+    };
+    return res.json(jsonResult);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+})
+
 module.exports = router;
