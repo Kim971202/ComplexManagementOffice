7dd1e5a133eee57ff2fa8d07a6dd8d4841cd2742
Kim971202 <kimyohan971202@gmail.com>
2022-09-27 08:52:26 +0900

    공지관련 수정,삭제 구현

--- routes/noticeRouter.js
+++ routes/noticeRouter.js
@@ -19,7 +19,14 @@ router.get("/getDetailedNoticeList", async (req, res, next) => {
                  INNER JOIN t_notice_send b
                  WHERE a.idx = b.idx AND a.idx = ?`;
     const data = await pool.query(sql, [idx]);
+    let test = data[0];
+    console.log(typeof test[0].endDate);
+    console.log(test[0].endDate.length);
 
+    let myTime = new Date();
+    let mySeconds = myTime.getSeconds();
+    console.log(typeof mySeconds.length);
+    // if(mySeconds.length)
     console.log("sql: " + sql);
     let resultList = data[0];
     let jsonResult = {
@@ -36,97 +43,73 @@ router.get("/getDetailedNoticeList", async (req, res, next) => {
   }
 });
 
+// TODO: LIMIT 추가
 // 공지사항 목록조회
 router.get("/getNoticeList", async (req, res, next) => {
   let {
-   startDate = "",
-   endDate = "",
-   notiType = "",
-   sendResult = "",
-   notiContent = ""
+    startDate = "",
+    endDate = "",
+    notiType = "",
+    sendResult = "",
+    notiContent = "",
   } = req.query;
 
-  console.log(
-    startDate, endDate, notiType, sendResult, notiContent
-  );
+  console.log(startDate, endDate, notiType, sendResult, notiContent);
 
-  try {    
+  try {
     let defaultCondition = `LIKE '%'`;
     let defaultNotiTypeCondtion = defaultCondition;
     let defaultSendResultCondition = defaultCondition;
     let defaultNotiContentCondtion = defaultCondition;
 
+    let defaultStartDate = "";
+    let defaultEndDate = "";
+
     let notiTypeCondition = "";
     let sendResultCondition = "";
-    let notiContentCondition = ""
+    let notiContentCondition = "";
 
-    if(!startDate){
-      let defaultStartDate = "1990-01-01";
+    if (!startDate) {
+      defaultStartDate = "1990-01-01";
     }
-    if(!endDate){
-      let defaultEndDate = "3000-01-01";
+    if (!endDate) {
+      defaultEndDate = "3000-01-01";
     }
-    if(!!notiType){
-      notiTypeCondition = `= '${notiType}'`
+    if (!!notiType) {
+      notiTypeCondition = `= '${notiType}'`;
       defaultNotiTypeCondtion = "";
     }
-    if(!!sendResult){
-      sendResultCondition = `= ${sendResult}`
+    if (!!sendResult) {
+      sendResultCondition = `= ${sendResult}`;
       defaultSendResultCondition = "";
     }
-    if(!!notiContent){
-      notiContentCondition = `= ${notiContent}`
-      defaultNotiContentCondtion = ""
+    if (!!notiContent) {
+      notiContentCondition = `= ${notiContent}`;
+      defaultNotiContentCondtion = "";
     }
 
-    let notiType_ = "%";
-    if (notiType === "1") notiType_ = "전체";
-    else if (notiType === "2") notiType_ = "개별";
-    console.log("notiType_=> " + notiType_);
-
-    const sql3 = `UPDATE t_notice a 
-                    INNER JOIN t_notice_send b 
+    const sql3 = `UPDATE t_notice a
+                    INNER JOIN t_notice_send b
                     ON a.idx = b.idx
                     SET a.new_flag = IF (DATE_ADD(a.start_date, INTERVAL 3 DAY)  >=now(), 'Y', 'N')
                     WHERE a.idx = b.idx;`;
     const data3 = await pool.query(sql3);
-    console.log("data3: " + data3);
 
-    const tSQL =
-      " and b.dong_code ='" + dongCode + "' and b.ho_code = '" + hoCode + "' ";
-
-    const checkingSQL = `SELECT IFNULL(MAX(a.idx), 'NORESULT') AS RESULT
-                         FROM t_notice a
-                         INNER JOIN t_notice_send b 
-                         WHERE a.idx = b.idx AND a.start_date <= now() AND end_date >= now() and a.noti_type LIKE ?  ${tSQL}
-                         ;`;
-
-    const sql = `SELECT ROW_NUMBER() OVER(ORDER BY idx) AS No, noti_type AS notiType, noti_title AS notiTitle, noti_owner AS notiOwner,
-                        DATE_FORMAT(start_date, '%Y-%m-%d') AS startDate,
-                        DATE_FORMAT(end_date, '%Y-%m-%d') AS endDate
-                        send_result AS sendResult
+    const sql = `SELECT ROW_NUMBER() OVER(ORDER BY a.idx) AS No, a.noti_type AS notiType, a.noti_title AS notiTitle, a.noti_owner AS notiOwner,
+                        DATE_FORMAT(a.start_date, '%Y-%m-%d') AS startDate,
+                        DATE_FORMAT(a.end_date, '%Y-%m-%d') AS endDate,
+                        b.send_result AS sendResult
                         FROM t_notice a
                         INNER JOIN t_notice_send b
                         WHERE a.idx = b.idx 
-                              AND (DATE(start_date) >= '${defaultStartDate} ${startDate}' AND (DATE(end_date) <= '${defaultEndDate} ${endDate}'))
-                              AND noti_type ${defaultCondition} ${notiTypeCondition}
-                              AND send_result ${defaultCondition} ${sendResultCondition}
-                              AND noti_content ${defaultNotiContentCondtion} ${notiContentCondition}`;
-                              
+                              AND (DATE(a.start_date) >= '${defaultStartDate} ${startDate}' AND (DATE(a.end_date) <= '${defaultEndDate} ${endDate}'))
+                              AND a.noti_type ${defaultNotiTypeCondtion} ${notiTypeCondition}
+                              AND b.send_result ${defaultSendResultCondition} ${sendResultCondition}
+                              AND a.noti_content ${defaultNotiContentCondtion} ${notiContentCondition}`;
+
     console.log("sql=>" + sql);
-    console.log("notiType_=> " + notiType_);
-    const data = await pool.query(sql, [notiType_, Number(sRow), Number(size)]);
-    const checkingData = await pool.query(checkingSQL, [notiType_]);
-
-    let result = checkingData[0];
+    const data = await pool.query(sql);
     let resultList = data[0];
-
-    const sql2 = `select count(a.idx) as cnt
-                   from t_notice a
-                   inner join  t_notice_send b 
-                   where a.idx = b.idx and a.start_date <= now() and end_date >= now() and a.noti_type LIKE ?  ${tSQL};`;
-    console.log("notiType_=> " + notiType_);
-    const data2 = await pool.query(sql2);
 
     let jsonResult = {
       resultCode: "00",
@@ -135,13 +118,21 @@ router.get("/getNoticeList", async (req, res, next) => {
         resultList,
       },
     };
-    // console.log(resultList);
-
+    console.log(resultList);
     return res.json(jsonResult);
   } catch (err) {
     return res.status(500).json(err);
   }
 });
+
+// const tSQL =
+//   " and b.dong_code ='" + dongCode + "' and b.ho_code = '" + hoCode + "' ";
+
+// const checkingSQL = `SELECT IFNULL(MAX(a.idx), 'NORESULT') AS RESULT
+//                      FROM t_notice a
+//                      INNER JOIN t_notice_send b
+//                      WHERE a.idx = b.idx AND a.start_date <= now() AND end_date >= now() and a.noti_type LIKE ?  ${tSQL}
+//                      ;`;
 
 // 공지사항 등록 (개별, 전체)
 router.post("/postNotice", async (req, res, next) => {
@@ -167,7 +158,7 @@ router.post("/postNotice", async (req, res, next) => {
   );
   try {
     let sql = `INSERT INTO t_notice(noti_type, noti_title, noti_content, start_date, end_date, noti_owner, insert_date, user_id, new_flag)
-               VALUES(?,?,?,DATE_FORMAT(?,"%y-%m-%d"),DATE_FORMAT(?,"%y-%m-%d"),?,now(),'8888','N')`;
+               VALUES(?,?,?,? + current_time(),DATE_FORMAT(?,"%y-%m-%d"),?,now(),'8888','N')`;
     console.log("sql=>" + sql);
     const data = await pool.query(sql, [
       notiType,
@@ -178,31 +169,34 @@ router.post("/postNotice", async (req, res, next) => {
       notiOwer,
     ]);
 
-    let countSQL = `SELECT ho_code AS hoCode, 
-                    (SELECT COUNT(ho_code) AS hCount FROM t_dongho WHERE dong_code = ?) AS hCount 
+    let countSQL = `SELECT ho_code AS hoCode, (SELECT COUNT(ho_code) AS hCount FROM t_dongho WHERE dong_code = ?) AS hCount 
                     FROM t_dongho WHERE dong_code = ?`;
+    console.log("countSQL: " + countSQL);
     const countData = await pool.query(countSQL, [dongCode, dongCode]);
-    console.log(countData[0])
-    
+    console.log(countData[0]);
+
     let getIdxSQL = `SELECT idx as idx FROM t_notice ORDER BY idx DESC LIMIT 1`;
     const getIdx = await pool.query(getIdxSQL);
     console.log("getIdx: " + getIdx[0][0].idx);
 
     let insertNoticeSendSQL = `INSERT INTO t_notice_send(idx, ho_code, dong_code, send_time, send_result)
                                VALUES(?,?,?,now(),'N')`;
-                              
-    if(notiType == "개별"){
+
+    if (notiType == "개별") {
       const noticeSendData = await pool.query(insertNoticeSendSQL, [
         getIdx[0][0].idx,
         hoCode,
         dongCode,
       ]);
-    } else if (notiType == "전체"){
-      for(i = 0; i < countData[0][0].hCount; i++){
-        const notiData = await pool.query(insertNoticeSendSQL, [getIdx[0][0].idx, countData[0][i].hoCode, dongCode]);
+    } else if (notiType == "전체") {
+      for (i = 0; i < countData[0][0].hCount; i++) {
+        const notiData = await pool.query(insertNoticeSendSQL, [
+          getIdx[0][0].idx,
+          countData[0][i].hoCode,
+          dongCode,
+        ]);
       }
-    }       
-
+    }
 
     let jsonResult = {
       resultCode: "00",
@@ -213,6 +207,71 @@ router.post("/postNotice", async (req, res, next) => {
   } catch (err) {
     console.log("test===============" + err);
     return res.status(500).json(err);
+  }
+});
+
+// 공지사항 수정
+router.put("/updateNotice", async (req, res, next) => {
+  let { idx = 0, notiTitle = "", notiContent = "" } = req.body;
+  console.log(idx, notiTitle, notiContent);
+
+  try {
+    // 월패드 알림이 Y 이면 수정 불가
+    const checkNotiTypeSQL = `SELECT send_result AS sendResult FROM t_notice_send WHERE idx = ?`;
+    console.log("checkNotiTypeSQL: " + checkNotiTypeSQL);
+    const data = await pool.query(checkNotiTypeSQL, [idx]);
+    console.log(data[0][0].sendResult);
+    if (data[0][0].sendResult === "Y") {
+      return res.json({
+        resultCode: "08",
+        resultMsg: "이미 등록된 공지사항 입니다.",
+      });
+    }
+
+    const sql = `UPDATE t_notice SET noti_title = ?, noti_content = ? WHERE idx = ?`;
+    console.log("sql: " + sql);
+    const data2 = await pool.query(sql, [notiTitle, notiContent, idx]);
+
+    let jsonResult = {
+      resultCode: "00",
+      resultMsg: "NORMAL_SERVICE",
+    };
+
+    return res.json(jsonResult);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// 공지사항 삭제
+router.delete("/deleteNotice", async (req, res) => {
+  let { idx = 0 } = req.body;
+  console.log(idx);
+
+  try {
+    // 월패드 알림이 Y 이면 삭제 불가
+    const checkNotiTypeSQL = `SELECT send_result AS sendResult FROM t_notice_send WHERE idx = ?`;
+    console.log("checkNotiTypeSQL: " + checkNotiTypeSQL);
+    const data = await pool.query(checkNotiTypeSQL, [idx]);
+    console.log(data[0][0].sendResult);
+    if (data[0][0].sendResult === "Y") {
+      return res.json({
+        resultCode: "08",
+        resultMsg: "이미 등록된 공지사항 입니다.",
+      });
+    }
+
+    const sql = `DELETE FROM t_notice_send WHERE idx = ?`;
+    console.log("sql: " + sql);
+    const data2 = await pool.query(sql, [idx]);
+
+    let jsonResult = {
+      resultCode: "00",
+      resultMsg: "NORMAL_SERVICE",
+    };
+    return res.json(jsonResult);
+  } catch (error) {
+    return res.status(500).json(error);
   }
 });
 
