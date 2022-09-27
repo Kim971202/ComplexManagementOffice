43d250c0e7613eb77bc75063cafc16fc4e70240f
Kim971202 <kimyohan971202@gmail.com>
2022-09-27 09:08:36 +0900

    주요 연락처 등록 구현

--- routes/keyContractRouter.js
+++ routes/keyContractRouter.js
@@ -2,20 +2,54 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../DB/dbPool");
 
+// 주요 연락처 조회
+router.get("/getKeyContract", async (req, res, next) => {
+  let { contactFlag = "" } = req.query;
+  console.log(contactFlag);
+
+  try {
+    let defaultCondition = `LIKE '%'`;
+    let contactFlagCondition = "";
+    if (!!contactFlag) {
+      contactFlagCondition = `= '${contactFlag}'`;
+      defaultCondition = "";
+    }
+    const sql = `SELECT ROW_NUMBER() OVER(ORDER BY idx) AS No, contact_flag AS contactFlag, facility_name AS facilityName, phone_num AS phoneNum,
+                        DATE_FORMAT(insert_dtime, '%Y-%m-%d') AS insertDTime
+                 FROM t_key_contact
+                 WHERE contact_flag ${defaultCondition} ${contactFlagCondition}`;
+    console.log("sql: " + sql);
+    const data = await pool.query(sql);
+    let resultList = data[0];
+
+    let jsonResult = {
+      resultCode: "00",
+      resultMsg: "NORMAL_SERVICE",
+      data: {
+        resultList,
+      },
+    };
+    return res.json(jsonResult);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// 주요 연락처 등록
 router.post("/postKeyContract", async (req, res, next) => {
   let {
-    contactFlage = "",
+    contactFlag = "",
     facilityName = "",
     phoneNum = "",
     memo = "",
   } = req.query;
-  console.log(contactFlage, facilityName, phoneNum, memo);
+  console.log(contactFlag, facilityName, phoneNum, memo);
 
   try {
     const sql = `INSERT INTO t_key_contact(phone_num, contact_flag, facility_name, memo, insert_dtime)
                  VALUES(?,?,?,?,now())`;
     const data = await pool.query(sql, [
-      contactFlage,
+      contactFlag,
       facilityName,
       phoneNum,
       memo,
